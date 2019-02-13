c3a253e2041f528a3c11fb53c803d4c62b81f60b
Gabriel Ungier <gabe@Gabriels-MacBook-Pro.local>
2019-02-12 19:07:34 -0500

    changed name of Variable wordSpace to wordLength

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -8,8 +8,8 @@
 var compList = ["Rick and Morty" , "Spongebob Squarepants" , "Adventure Time" , "Gravity Falls" , "ThunderCats" , "Phineas and Ferb" , "Regular Show" , "Avatar" , "Legend of Korra" , "Samurai Jack" , "Cowboy Bebop" , "Dexters Laboratory" , "Simpsons" , "Teen Titans" , "He Man" , "Larva"]
 var compChoose = "";            //random picked word stored here
 var lettersINchoice = [];       //use to break letters from word into new array of letters
-var wordSpace = 0 ;             //amount of blank tiles word will take
-var rightANDleft = [];           //Variable to hold the right answers and the remaining blank tiles
+var wordLength = 0 ;             //amount of blank tiles word will take
+var correctNremaining = [];           //Variable to hold the right answers and the remaining blank tiles
 var userGuess = "" ;            //place to hold ALL letters guessed and convert them to recognizable case
 var userWrong = [] ;            //holds all the wrong letters guessed
 
@@ -33,21 +33,21 @@ function begin (){
     
     lettersINchoice = compChoose.split("");  //should break word into individual letter array
 console.log(lettersINchoice);
-    wordSpace = lettersINchoice.length;  //should count the letters from last array
-console.log(wordSpace);
-    rightANDleft = [];  //look at wordSpace and alter it based on guesses
+    wordLength = lettersINchoice.length;  //should count the letters from last array
+console.log(wordLength);
+    correctNremaining = [];  //look at wordLength and alter it based on guesses
 
     userWrong = []; //
 
-    for (var x = 0; x < wordSpace; x++) {
-        rightANDleft.push("_");  //please create the correct number of blank spaces from the lettersINchoice array *\./*
+    for (var x = 0; x < wordLength; x++) {
+        correctNremaining.push("_");  //please create the correct number of blank spaces from the lettersINchoice array *\./*
 
     }
 console.log(compChoose); 
-console.log(rightANDleft);
+console.log(correctNremaining);
 
-document.getElementById("blankTiles").innerHTML = rightANDleft.join(" ").replace(/,/g,"");
-console.log("inside for loop:" + rightANDleft);
+document.getElementById("blankTiles").innerHTML = correctNremaining.join(" ").replace(/,/g,"");
+
 document.getElementById("wrongGuess").innerHTML = userWrong.join(" ");
 
 document.getElementsByClassName("guessTimates").innerHTML = guessCounter;
@@ -60,7 +60,7 @@ function inspector (letter) {
 
     var inWord = false;  
 
-    for (var x = 0; x < wordSpace; x++) {
+    for (var x = 0; x < wordLength; x++) {
 
         if (compChoose[x] === letter) {  
 
@@ -70,13 +70,13 @@ function inspector (letter) {
 
 
 
- if (inWord) {  //this figures out the WHERE of the letters especially with the variable rightANDleft
+ if (inWord) {  //this figures out the WHERE of the letters especially with the variable correctNremaining
 
-    for (var y = 0; y < wordSpace; y++) {
+    for (var y = 0; y < wordLength; y++) {
 
         if (compChoose[y] === letter) {
 
-            rightANDleft[y] = letter;
+            correctNremaining[y] = letter;
         }
     }
 
@@ -99,11 +99,11 @@ function roundOver() {
 
     document.getElementById("guessTimates").innerHTML = guessCounter; //changes to HTML to display as guesses change
 
-    document.getElementById("blankTiles").innerHTML = rightANDleft; //appends the array 
+    document.getElementById("blankTiles").innerHTML = correctNremaining; //appends the array 
 
     document.getElementById("wrongGuess").innerHTML = userWrong; //so the user can see what letters they've tried
 
-if (lettersINchoice.toString() === rightANDleft.toString()) {
+if (lettersINchoice.toString() === correctNremaining.toString()) {
 
     wins ++ ; //add to number of wins
         
